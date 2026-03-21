9875434b69cd6782fcc875e20b0cc2d5230a89f9
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-03-21 18:28:33 +0530

    done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      login
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
 
-export default Login
+  const handleSumit = (e) => {
+    e.preventDefault();
+    console.log(email)
+  };
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form
+        className="bg-white p-6 rounded shadow-md w-full md:w-[500px]"
+        onSubmit={handleSumit}
+      >
+        <h2 className="text-2xl mg-4">Login</h2>
+        <div className="mb-4">
+          <label className="block text-gray-700 mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border p-2 w-full rounded outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Verify = () => {
+  const [otp, setOtp] = useState("");
+  
+    const handleSumit = (e) => {
+      e.preventDefault();
+      console.log(otp)
+    };
   return (
-    <div>
-      verify
+    <div className="flex justify-center items-center h-screen">
+      <form
+        className="bg-white p-6 rounded shadow-md w-full md:w-[500px]"
+        onSubmit={handleSumit}
+      >
+        <h2 className="text-2xl mg-4">Verify</h2>
+        <div className="mb-4">
+          <label className="block text-gray-700 mb-2" htmlFor="otp">
+            OTP
+          </label>
+          <input
+            type="number"
+            id="otp"
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+            className="border p-2 w-full rounded outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
+          Submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Verify
+export default Verify;
